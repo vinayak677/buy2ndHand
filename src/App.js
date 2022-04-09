a732663549e56d79c4b53909b1b6c14338b4ad3f
vinayak677 <vinayak3friends@gmail.com>
2022-04-09 13:01:13 +0530

    removed login page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Navbar from './Component/Navbar/Navbar';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
-import Login from './Pages/Login';
-import Footer from './Component/Footer';
 
 function App() {
   return (
@@ -12,7 +10,6 @@ function App() {
     <Navbar/>
     <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
     </Routes>
     <Footer/>
     </div>
--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -7,10 +7,12 @@ const Navbar = () => {
   const [search,setSearch]=useState('')
   return (
     <div className='main'>
-      <div className="navbar">
-          <Link className='left' to="/"><i class="fa fa-fw fa-home"></i> Home</Link>
-          <input className='search-bar' value={search} SearchOutlined type='text' placeholder='search'onSubmit={(e)=>setSearch(e.preventDefault())}/>
-          <Link className='right' to="login"><i class="fa fa-fw fa-user"></i> Login</Link>
+      <div className="buy2hand-container">
+        <div className='buy2hand-container-content'>
+            <Link className='buy2hand-container-content-home' to="/"><i class="fa fa-fw fa-home"></i> Home</Link>
+            <input className='buy2hand-container-content-search' value={search} SearchOutlined type='text' placeholder='search'onChange={(e)=>setSearch(e.target.value)}/>
+        </div>
+        <Link className='buy2hand-container-content-login' to="login"><i class="fa fa-fw fa-user"></i> Login</Link>
       </div>
     </div>
 
