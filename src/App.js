c9cf8a38587d669860b27d116b430cf6c04b45bf
vinayak677 <vinayak3friends@gmail.com>
2022-04-10 02:37:50 +0530

    added option and tags

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 
+
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <Routes>
         <Route path="/" element={<Home />} />
+        {/* <Route path="/sell" element={<Sell />} />
+        <Route path="/login" element={<Login />} /> */}
     </Routes>
-    <Footer/>
     </div>
   );
 }
